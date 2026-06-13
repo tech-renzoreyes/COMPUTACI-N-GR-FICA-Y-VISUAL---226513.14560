3b117afca2b828f74af0f70611c2c139e16b214d
Khriz_acm <acm.99@outlook.com>
2026-06-12 23:09:51 -0500

    READO MODO TURBO

--- PC_4/PC_4/PC_4.cpp
+++ PC_4/PC_4/PC_4.cpp
@@ -1,124 +1,152 @@
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
-#include <iostream>
-#include <cmath>
-#include "src/Shader.h"
+
+#include <GL/glew.h> 
+#include <GLFW/glfw3.h> 
+#include <iostream> 
+#include "src/Shader.h" 
+
+//  NAVE (2 TRIÁNGULOS) 
 float vertices[] =
 {
-	//Posición //Color
-	-0.5f,-0.5f,0.0f, 1.0f,0.0f,0.0f,
-	0.5f,-0.5f,0.0f, 0.0f,1.0f,0.0f,
-	0.0f, 0.5f,0.0f, 0.0f,0.0f,1.0f,
+    // Punta superior
+ 0.0f,  0.48f, 0.0f,   0.0f,0.5f,1.0f,
+-0.15f, 0.21f, 0.0f,   0.0f,0.5f,1.0f,
+ 0.15f, 0.21f, 0.0f,   0.0f,0.5f,1.0f,
+
+ // Cuerpo inferior
+ -0.15f, 0.21f, 0.0f,   0.0f,0.5f,1.0f,
+  0.15f, 0.21f, 0.0f,   0.0f,0.5f,1.0f,
+  0.0f, -0.45f, 0.0f,   0.0f,0.5f,1.0f,
+
+  // Ala izquierda
+  -0.15f, 0.15f, 0.0f,   0.0f,0.5f,1.0f,
+  -0.54f,-0.03f, 0.0f,   0.0f,0.5f,1.0f,
+   0.00f,-0.06f, 0.0f,   0.0f,0.5f,1.0f,
+
+   // Ala derecha
+    0.15f, 0.15f, 0.0f,   0.0f,0.5f,1.0f,
+    0.54f,-0.03f, 0.0f,   0.0f,0.5f,1.0f,
+    0.00f,-0.06f, 0.0f,   0.0f,0.5f,1.0f,
 };
-// Variables para las posiciones
+
+
+
+
 float posX = 0.0f;
 float posY = 0.0f;
+
 float deltaTime = 0.0f;
 float lastFrame = 0.0f;
+
+bool turbo = false;
+
 void responsive(GLFWwindow* window, int width, int height);
 void userInput(GLFWwindow* window);
-void mouse_cursor_position(GLFWwindow* window, double xpos, double ypos);
-void mouse_scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+
 int main()
 {
-	//Inicializar GLFW
-	glfwInit();
-	//Inicializar la version
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-#ifdef __APPLE__
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-#endif
-	//Crear ventana
-	GLFWwindow* window = glfwCreateWindow(1200, 800, "Mi primera ventana - Cristian mi rama", NULL, NULL);
-	//Validar la ventana
-	if (!window)
-	{
-		std::cout << "Error en al inicialziación la ventana\n";
-		glfwTerminate();
-	}
-	glfwMakeContextCurrent(window);
-	glfwSetFramebufferSizeCallback(window, responsive);
-	glfwSetCursorPosCallback(window, mouse_cursor_position);
-	glfwSetScrollCallback(window, mouse_scroll_callback);
-	if (glewInit() != GLEW_OK)
-	{
-		std::cout << "Failed to initialize Glew\n";
-		glfwTerminate();
-	}
-	//Shader
-	Shader myShader("res/Shader/vertexShader.glsl", "res/Shader/fragmentShader.glsl");
-	//Buffers
-	unsigned int VBO, VAO;
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
-	glBindVertexArray(VAO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), &vertices, GL_STATIC_DRAW);
-	//Vertices
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
-	//Colores
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
-	glEnableVertexAttribArray(1);
-	//Iniciamos
-	while (!glfwWindowShouldClose(window))
-	{
-		//DeltaTime - Cambio del FPS
-		float currentFrame = glfwGetTime();
-		deltaTime = currentFrame - lastFrame;
-		lastFrame = currentFrame;
-		//Control de Tecla
-		userInput(window);
-		//actualización de tiempos y colores
-		float time = glfwGetTime();
-		float xValue = std::cos(time) / 2.0f + 0.5f;
-		// float yValue = std::sin(time) / 2.0f + 0.5f;
-		// float zValue = std::cos(time) / 2.0f + 0.5f;
-		myShader.use();
-		myShader.setFloat("xColor", xValue);
-		//Actualizar Posición
-		myShader.setFloat("xOffset", posX);
-		myShader.setFloat("yOffset", posY);
-		//Render - Color
-		glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
-		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
-		// Traspaso de la pantalla
-		glfwSwapBuffers(window);
-		// cambio de eventos (congela)
-		glfwPollEvents();
-	}
-	//limpiar
-	glfwDestroyWindow(window);
-	glfwTerminate();
+    glfwInit();
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Nave Espacial", NULL, NULL);
+
+    if (!window)
+    {
+        std::cout << "Error creando ventana\n";
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, responsive);
+
+    if (glewInit() != GLEW_OK)
+    {
+        std::cout << "Error GLEW\n";
+        return -1;
+    }
+
+    Shader myShader("res/Shader/vertexShader.glsl", "res/Shader/fragmentShader.glsl");
+
+    unsigned int VBO, VAO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // Posición 
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // Color 
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 *
+        sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        float currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+
+        userInput(window);
+
+        myShader.use();
+
+        // Movimiento 
+        myShader.setFloat("xOffset", posX);
+        myShader.setFloat("yOffset", posY);
+
+        // Turbo 
+        myShader.setFloat("turboActivo", turbo ? 1.0f : 0.0f);
+
+        glClearColor(0.05f, 0.05f, 0.1f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 12); //NUMERO DE VERTICES
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
 }
+
 void responsive(GLFWwindow* window, int width, int height)
 {
-	glViewport(0, 0, width, height);
+    glViewport(0, 0, width, height);
 }
+
 void userInput(GLFWwindow* window)
 {
-	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-		glfwSetWindowShouldClose(window, GLFW_TRUE);
-	//Movimiento
-	float cameraSpeed = 2.5f * deltaTime;
-	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-		posY = posY + cameraSpeed; // Arriba
-	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-		posY -= cameraSpeed; // Abajo
-	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-		posX -= cameraSpeed; // Izquierda
-	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-		posX += cameraSpeed; // Derecha
-}
-void mouse_cursor_position(GLFWwindow* window, double xpos, double ypos)
-{
-	// std::cout << "Posición del Mouse :" << xpos << " " << ypos << std::endl;
-}
-void mouse_scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
-{
-	//std::cout << "Valor del Scroll: " << xoffset << " " << yoffset << std::endl;
+    float cameraSpeed;
+
+    //  TURBO 
+    if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS)
+    {
+        cameraSpeed = 5.0f * deltaTime;
+        turbo = true;
+    }
+    else
+    {
+        cameraSpeed = 2.5f * deltaTime;
+        turbo = false;
+    }
+
+    // Movimiento 
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) posY += cameraSpeed;
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) posY -= cameraSpeed;
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) posX -= cameraSpeed;
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) posX += cameraSpeed;
+
+    //  Efecto Pac-Man 
+    if (posX > 1.0f) posX = -1.0f;
+    if (posX < -1.0f) posX = 1.0f;
+
+    if (posY > 1.0f) posY = -1.0f;
+    if (posY < -1.0f) posY = 1.0f;
 }
--- PC_4/PC_4/res/Shader/fragmentShader.glsl
+++ PC_4/PC_4/res/Shader/fragmentShader.glsl
@@ -1,12 +1,18 @@
-#version 330 core
-
-out vec4 FragColor;
-
-in vec3 ourColor;
-
-uniform float xColor;
-
-void main()
-{
-	FragColor = vec4(xColor * ourColor.x, ourColor.y,  ourColor.z ,1.0f);
-}
+#version 330 core 
+ 
+out vec4 FragColor; 
+in vec3 ourColor; 
+ 
+uniform float turboActivo; //variable qu hara cambiar de color a la nave
+ 
+void main() 
+{ 
+    if(turboActivo == 1.0) 
+    { 
+        FragColor = vec4(1.0, 0.1, 0.1, 1.0); // rojo 
+    } 
+    else 
+    { 
+        FragColor = vec4(0.1, 0.3, 1.0, 1.0); // azul 
+    } 
+ } 
