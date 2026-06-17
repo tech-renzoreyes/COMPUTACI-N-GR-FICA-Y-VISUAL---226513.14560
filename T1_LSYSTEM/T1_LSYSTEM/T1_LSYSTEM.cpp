f460965ad7798d1075e6a10527c8eb77e893a89f
Khriz_acm <acm.99@outlook.com>
2026-06-17 00:18:09 -0500

    2 Arboles con tronco marron ramas verdes

--- T1_LSYSTEM/T1_LSYSTEM/T1_LSYSTEM.cpp
+++ T1_LSYSTEM/T1_LSYSTEM/T1_LSYSTEM.cpp
@@ -1,148 +1,472 @@
 #include <GL/glew.h>
+
 #include <GLFW/glfw3.h>
+
 #include <iostream>
+
 #include <cmath>
+
 #include <vector>
+
 #include <string>
+
 #include <stack>
+
 #include "src/Shader.h"
-// Variables para las posiciones
-float posX = 0.0f;
-float posY = -0.8f; // Empezamos un poco más abajo para ver el árbol
-float deltaTime = 0.0f;
-float lastFrame = 0.0f;
+
+
+
+// Variables para las posiciones y tiempo
+
+float posX = 0.0f;  // Movimiento horizontal de la escena
+
+float posY = -0.8f; // Movimiento vertical
+
+float deltaTime = 0.0f; // Tiempo entre frames
+
+float lastFrame = 0.0f; // Último frame registrado
+
+
+
 void responsive(GLFWwindow* window, int width, int height);
+
 void userInput(GLFWwindow* window);
-// L-System - Recursividad
+
+
+//L-SYSTEM arbol
+
 std::string generateLSystem(std::string axiom, int iterations) {
-	std::string current = axiom;
-	for (int i = 0; i < iterations; i++) {
-		std::string next = "";
-		for (char c : current) {
-			if (c == 'F') {
-				// Regla principal del árbol fractal
-				next += "F[+F]F[-F][F]";
-			}
-			else {
-				next += c;
-			}
-		}
-		current = next;
-	}
-	return current;
+
+    std::string current = axiom;
+
+    for (int i = 0; i < iterations; i++) {
+
+        std::string next = "";
+
+        for (char c : current) {
+
+            if (c == 'F') {
+
+                // Regla principal del árbol fractal
+
+                next += "FF+[+F-F+F]-[-F+F-F]"; //tipo de arbol
+
+                //next += "FF+[+F-F]-[-F+F]";   //tipo de arbol
+
+
+                //next += "FF+[+F-F-F]-[-F+F+F]";   //tipo de arbol
+
+                 
+            }
+
+            else {
+
+                next += c;
+
+            }
+
+        }
+        current = next;
+    }
+    return current;
+
 }
-// Variables para las ramas
+
+
+//Variables para las ramas
+
 struct TurtleState {
-	float x, y, angle;
+    float x, y, angle;
+    int depth;
 };
-// Generación de los vértices
-std::vector<float> generateVertices(const std::string& sentence, float length, float angleDeg) {
-	std::vector<float> vertices;
-	std::stack<TurtleState> stack;
-	float x = 0.0f;
-	float y = 0.0f;
-	float angle = 90.0f; // Mirando hacia arriba
-	// Convertir a radianes para math.h
-	float angleRad = angle * (3.14159265359f / 180.0f);
-	for (char c : sentence) {
-		if (c == 'F') {
-			// Calcular nueva posición usando trigonometría
-			float newX = x + length * std::cos(angleRad);
-			float newY = y + length * std::sin(angleRad);
-			// Agregar vértice inicial (Posición + Color Verde)
-			vertices.push_back(x); vertices.push_back(y); vertices.push_back(0.0f);
-			vertices.push_back(0.2f); vertices.push_back(0.8f); vertices.push_back(0.2f);
-			// Agregar vértice final (Posición + Color Verde)
-			vertices.push_back(newX); vertices.push_back(newY); vertices.push_back(0.0f);
-			vertices.push_back(0.2f); vertices.push_back(0.8f); vertices.push_back(0.2f);
-			x = newX;
-			y = newY;
-		}
-		else if (c == '+') {
-			angle -= angleDeg;
-			angleRad = angle * (3.14159265359f / 180.0f);
-		}
-		else if (c == '-') {
-			angle += angleDeg;
-			angleRad = angle * (3.14159265359f / 180.0f);
-		}
-		else if (c == '[') {
-			stack.push({ x, y, angle });
-		}
-		else if (c == ']') {
-			TurtleState state = stack.top();
-			stack.pop();
-			x = state.x;
-			y = state.y;
-			angle = state.angle;
-			angleRad = angle * (3.14159265359f / 180.0f);
-		}
-	}
-	return vertices;
+
+
+// VÉRTICES DEL ÁRBOL
+
+std::vector<float> generateVertices(
+    const std::string& sentence,
+    float length,
+    float angleDeg,
+    float startAngle
+) {
+
+    std::vector<float> vertices;
+    std::stack<TurtleState> stack;
+
+
+    float x = 0.0f;
+    float y = 0.0f;
+
+    // Dirección inicial del árbol
+    float angle = startAngle;
+
+    int depth = 0;
+
+    float angleRad = angle * (3.14159265359f / 180.0f);
+
+
+    for (char c : sentence) {
+
+        if (c == 'F') {
+
+            // dirección actual
+            float newX = x + length * std::cos(angleRad);
+            float newY = y + length * std::sin(angleRad);
+
+            // Viento para hojas
+            float windEffect = std::sin(glfwGetTime() * 2.0f);
+
+            float r, g, b;
+            float thickness = 1.0f;
+
+            // TRONCO Y RAMAS
+            // =========================
+            if (depth < 2.5) //cantidad de tronco definido en 2.5
+            {
+
+    
+                r = 0.25f; //color marron del tronco
+                g = 0.12f;
+                b = 0.03f;
+
+                thickness = 3.0f - depth;
+            }
+
+         
+            // HOJAS
+            // =========================
+            else {
+
+                r = 0.1f;
+                g = 0.8f;
+                b = 0.1f;
+
+                float move = windEffect * 0.02f;
+
+                vertices.push_back(newX + move);
+                vertices.push_back(newY);
+                vertices.push_back(0.0f);
+
+                vertices.push_back(r);
+                vertices.push_back(g);
+                vertices.push_back(b);
+
+            }
+
+            
+            // GROSOR DE TRONCO Y RAMAS
+            // =========================
+            for (int i = -thickness; i <= thickness; i++) {
+
+
+                float offset = i * 0.002f;
+
+
+                // punto inicial
+
+                vertices.push_back(x + offset);
+                vertices.push_back(y);
+                vertices.push_back(0.0f);
+
+                vertices.push_back(r);
+                vertices.push_back(g);
+                vertices.push_back(b);
+
+
+
+                // punto final
+
+                vertices.push_back(newX + offset);
+                vertices.push_back(newY);
+                vertices.push_back(0.0f);
+
+                vertices.push_back(r);
+                vertices.push_back(g);
+                vertices.push_back(b);
+
+            }
+
+            x = newX;
+            y = newY;
+
+        }
+
+     
+        // ROTAR DERECHA
+        // =========================
+        else if (c == '+') {
+
+            angle -= angleDeg;
+
+            angleRad =
+                angle * (3.14159265359f / 180.0f);
+
+        }
+
+        
+        // ROTAR IZQUIERDA
+        // =========================
+        else if (c == '-') {
+
+            angle += angleDeg;
+
+            angleRad =
+                angle * (3.14159265359f / 180.0f);
+
+        }
+
+        // GUARDAR ESTADO
+        // =========================
+        else if (c == '[') {
+
+            stack.push(
+                { x, y, angle, depth }
+            );
+            depth++;
+        }
+
+
+        // RESTAURAR ESTADO
+        // =========================
+        else if (c == ']') {
+
+
+            TurtleState state = stack.top();
+
+            stack.pop();
+
+            x = state.x;
+            y = state.y;
+            angle = state.angle;
+            depth = state.depth;
+            angleRad =
+                angle * (3.14159265359f / 180.0f);
+
+        }
+
+    }
+
+    return vertices;
+
 }
+
+
 int main()
+
 {
-	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	GLFWwindow* window = glfwCreateWindow(1200, 800, "L-System Fractal", NULL, NULL);
-	if (!window) { std::cout << "Error\n"; glfwTerminate(); return -1; }
-	glfwMakeContextCurrent(window);
-	glfwSetFramebufferSizeCallback(window, responsive);
-	if (glewInit() != GLEW_OK) { std::cout << "Error Glew\n"; glfwTerminate(); return -1; }
-	Shader myShader("res/Shader/vertexShader.glsl", "res/Shader/fragmentShader.glsl");
-	// Generamos las instrucciones del fractal una sola vez (4 iteraciones)
-	std::string fractalInstructions = generateLSystem("F", 4);
-	unsigned int VBO, VAO;
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
-	glBindVertexArray(VAO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	// Posición
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
-	// Color
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
-	glEnableVertexAttribArray(1);
-	while (!glfwWindowShouldClose(window))
-	{
-		float currentFrame = glfwGetTime();
-		deltaTime = currentFrame - lastFrame;
-		lastFrame = currentFrame;
-		userInput(window);
-		// Calculamos una oscilación basada en el tiempo actual usando seno.
-		float windEffect = std::sin(currentFrame * 1.5f) * 2.5f;
-		float dynamicAngle = 25.0f + windEffect; // El ángulo base de 25° ahora oscilará
-		// Generamos los vértices con el nuevo ángulo iterado
-		std::vector<float> vertices = generateVertices(fractalInstructions, 0.05f, dynamicAngle);
-		// Actualizamos el VBO en cada frame usando GL_DYNAMIC_DRAW
-		glBindBuffer(GL_ARRAY_BUFFER, VBO);
-		glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), vertices.data(), GL_DYNAMIC_DRAW);
-		// ----------------------------------------------
-		myShader.use();
-		myShader.setFloat("xOffset", posX);
-		myShader.setFloat("yOffset", posY);
-		glClearColor(0.05f, 0.05f, 0.05f, 1.0f); // Fondo oscuro
-		glClear(GL_COLOR_BUFFER_BIT);
-		glBindVertexArray(VAO);
-		// El número de vértices es el tamaño del vector / 6 datos por vértice
-		glDrawArrays(GL_LINES, 0, vertices.size() / 6);
-		glfwSwapBuffers(window);
-		glfwPollEvents();
-	}
-	glfwDestroyWindow(window);
-	glfwTerminate();
+    // Inicializa OpenGL
+    glfwInit();
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+
+    // Crear ventana
+    GLFWwindow* window = glfwCreateWindow(1200, 800, "T1 - LSYSTEM", NULL, NULL);
+
+    if (!window) { std::cout << "Error\n"; glfwTerminate(); return -1; }
+
+
+
+    glfwMakeContextCurrent(window);
+
+    glfwSetFramebufferSizeCallback(window, responsive);
+
+    if (glewInit() != GLEW_OK) { std::cout << "Error Glew\n"; glfwTerminate(); return -1; }
+
+
+    Shader myShader("res/Shader/vertexShader.glsl", "res/Shader/fragmentShader.glsl");
+
+
+    //// Árbol grande (ramas hacia la derecha)
+    //std::string treeBigInstructions = generateLSystem("F", 4);
+
+    //std::vector<float> treeBig =
+    //    generateVertices(
+    //        treeBigInstructions,
+    //        0.040f,
+    //        22.0f,
+    //        90.0f
+    //    );
+
+
+    //// Árbol mediano (ramas hacia la izquierda)
+    //std::string treeMediumInstructions = generateLSystem("F", 4);
+
+    //std::vector<float> treeMedium =
+    //    generateVertices(
+    //        treeMediumInstructions,
+    //        0.038f,
+    //        32.0f,
+    //        270.0f
+    //    );
+
+   
+
+    // Buffers
+    unsigned int VBO, VAO;
+
+    glGenVertexArrays(1, &VAO);
+
+    glGenBuffers(1, &VBO);
+
+
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  
+    // Posición
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+
+    glEnableVertexAttribArray(0);
+
+    // Color
+
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+
+    glEnableVertexAttribArray(1);
+
+    glEnable(GL_PROGRAM_POINT_SIZE);
+
+
+    // LOOP PRINCIPAL
+
+    while (!glfwWindowShouldClose(window))
+    {
+        float currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+
+        userInput(window);
+
+        myShader.use();
+        myShader.setFloat("xOffset", posX);
+        myShader.setFloat("yOffset", posY);
+
+        //glClearColor(0.60f, 0.85f, 1.00f, 1.0f); //azul claro
+        glClearColor(0.01f, 0.03f, 0.12f, 1.0f); //azul oscuro
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glBindVertexArray(VAO);
+
+        // Viento
+        float wind = std::sin(glfwGetTime() * 1.5f) * 5.0f;
+
+
+
+        // Árbol grande
+        std::vector<float> treeBig =
+            generateVertices(
+                generateLSystem("F", 4),
+                0.016f,
+                20.0f,
+                90.0f
+               
+            );
+
+
+        // Árbol con ramas hacia la izquierda
+        std::vector<float> treeMedium =
+            generateVertices(
+                generateLSystem("F", 4),
+                0.014f,
+                20.0f,
+                90.0f
+              
+            );
+
+
+
+        //// Árbol grande (normal)
+        //std::vector<float> treeBig =
+        //    generateVertices(
+        //        generateLSystem("F", 4), //cantodad de ramas
+        //        0.020f,
+        //        20.0f,
+        //        90.0f
+        //    );
+
+
+        //// Árbol con ramas al lado izquierdo
+        //std::vector<float> treeMedium =
+        //    generateVertices(
+        //        generateLSystem("F", 4), //cantodad de ramas
+        //        0.018f,
+        //        -35.0f,
+        //        90.0f
+        //    );
+
+        // ---- ÁRBOL 1 ----
+        myShader.setFloat("xOffset", -0.15f);//posicion de los arboles
+        myShader.setFloat("yOffset", -0.2f);
+
+        glBufferData(GL_ARRAY_BUFFER,
+            treeBig.size() * sizeof(float),
+            treeBig.data(),
+            GL_DYNAMIC_DRAW);
+
+        glDrawArrays(GL_LINES, 0, treeBig.size() / 6);
+
+
+        // ---- ÁRBOL 2 ----
+        myShader.setFloat("xOffset", 0.15f); //posicion de los arboles
+        myShader.setFloat("yOffset", -0.2f);
+
+        glBufferData(GL_ARRAY_BUFFER,
+            treeMedium.size() * sizeof(float),
+            treeMedium.data(),
+            GL_DYNAMIC_DRAW);
+
+        glDrawArrays(GL_LINES, 0, treeMedium.size() / 6);
+
+
+
+        glBufferData(GL_ARRAY_BUFFER,
+            treeMedium.size() * sizeof(float),
+            treeMedium.data(),
+            GL_DYNAMIC_DRAW);
+
+        glDrawArrays(GL_LINES, 0, treeMedium.size() / 6);
+        
+        glBufferData(GL_ARRAY_BUFFER,
+            treeMedium.size() * sizeof(float),
+            treeMedium.data(),
+            GL_DYNAMIC_DRAW);
+
+        glDrawArrays(GL_LINES, 0, treeMedium.size() / 6);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
 }
-void responsive(GLFWwindow* window, int width, int height) {
-	glViewport(0, 0, width, height);
+
+
+
+
+
+void responsive(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
 }
-void userInput(GLFWwindow* window) {
-	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-		glfwSetWindowShouldClose(window, GLFW_TRUE);
-	float cameraSpeed = 1.5f * deltaTime;
-	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) posY -= cameraSpeed;
-	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) posY += cameraSpeed;
-	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) posX += cameraSpeed;
-	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) posX -= cameraSpeed;
+
+void userInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+
+    float cameraSpeed = 1.5f * deltaTime;
+
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) posY -= cameraSpeed;
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) posY += cameraSpeed;
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) posX += cameraSpeed;
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) posX -= cameraSpeed;
 }
--- T1_LSYSTEM/T1_LSYSTEM/res/Shader/fragmentShader.glsl
+++ T1_LSYSTEM/T1_LSYSTEM/res/Shader/fragmentShader.glsl
@@ -1,12 +1,22 @@
-#version 330 core
+//#version 330 core
+//
+//out vec4 FragColor;
+//
+//in vec3 ourColor;
+//
+//uniform float xColor;
+//
+//void main()
+//{
+//	FragColor = vec4(xColor * ourColor.x, ourColor.y,  ourColor.z ,1.0f);
+//}
 
+#version 330 core
 out vec4 FragColor;
 
 in vec3 ourColor;
 
-uniform float xColor;
-
 void main()
 {
-	FragColor = vec4(xColor * ourColor.x, ourColor.y,  ourColor.z ,1.0f);
+    FragColor = vec4(ourColor, 1.0);
 }
